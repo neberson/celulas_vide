ffbf224b0eb4db9022848da9b4143056914903d4
Cristian Peres <christyansoftperes@gmail.com>
2020-09-08 22:43:46 -0300

    Criando página de perfil do discipulador

---
+++ lib/Discipulador/PerfilDiscipulador.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PerfilDiscipulador extends StatefulWidget {
+  @override
+  _PerfilDiscipuladorState createState() => _PerfilDiscipuladorState();
+}
+
+class _PerfilDiscipuladorState extends State<PerfilDiscipulador> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil do Discipulador'),
+      ),
+    );
+  }
+}
